5519a451a621c501fcb72a86d9a72e34fcaf6768
Luis Rodero-Merino <luis.rod@gmail.com>
2017-05-21 15:04:00 +0200

    We keep working on it...

--- src/main/scala/com/qvantel/miner/Main.scala
+++ src/main/scala/com/qvantel/miner/Main.scala
@@ -4,7 +4,7 @@ import org.rogach.scallop._
 import scala.util.{Try, Success, Failure}
 
 object Conf {
-  val DEFAULT_FIELD_SEPARATOR = '|'
+  val DEFAULT_FIELD_SEPARATOR = "|"
   val DEFAULT_PATHS_TO_MINE = "." :: Nil
 }
 
@@ -16,25 +16,25 @@ class Conf(args: Seq[String]) extends ScallopConf(args) {
   banner("""
     |Utility to mine xdr files.
     |Usage:
-    |       mine [opts] --for <regexp to mine>
+    |       mine [opts] --regexp <regexp to mine>
     |Examples:
-    |       mine --names "**.add" --for 1234 // recursively lookup for all *.add files from current folder containing 1234
-    |       mine --names "*.add" --for 1234  // lookup for all *.add files in current folder containing 1234
-    |       mine --cols 0 1 --names "**.add" --for 1234  // lookup for all *.add files in current folder containing 1234 in their two first columns
+    |       mine --names "**.add" --regexp 1234 // recursively lookup for all *.add files from current folder containing 1234
+    |       mine --names "*.add" --regexp 1234  // lookup for all *.add files in current folder containing 1234
+    |       mine --cols 0 1 --names "**.add" --regexp 1234  // lookup for all *.add files in current folder containing 1234 in their two first columns
     """.stripMargin)
   val help = opt[Boolean](noshort=true, default=Some(false), descr="Print this message and quit.")
-  val sep = opt[Char](noshort=true, default=Some(DEFAULT_FIELD_SEPARATOR), descr=s"Fields separator, defaults to '$DEFAULT_FIELD_SEPARATOR'.", argName="sep")
-  val print = opt[Boolean](noshort=true, default=Some(false), descr="Print matching lines to sdtout.")
-  val printWithFileName = opt[Boolean](noshort=true, default=Some(true), descr="Print matching lines to sdtout, appending to each line the file name.")
-  val copyTo = opt[String](noshort=true, descr="Copy matching files to this folder.", argName="folder")
-  val copyFilteredTo = opt[String](noshort=true, descr="Copy matching files to  this folder, but only write matching lines.", argName="folder")
-  val forr = opt[String](noshort=true, name="for", required=true, descr="Regexp to mine (required).", argName="regexp")
+  val sep = opt[String](noshort=true, default=Some(DEFAULT_FIELD_SEPARATOR), descr=s"Fields separator, defaults to '$DEFAULT_FIELD_SEPARATOR'.", argName="sep")
+  val copyTo = opt[String](noshort=true, name="copyTo", descr="Copy matching files to this folder.", argName="folder")
+  val copyFilteredTo = opt[String](noshort=true, name="copyFilteredTo", descr="Copy matching files to this folder, but only write matching lines.", argName="folder")
+  val regexp = opt[String](noshort=true, required=true, descr="Regexp to mine (required).", argName="regexp")
   val cols = opt[List[Int]](noshort=true, descr=s"List of columns to check in each row agains the pattern. If no set all cols are checked.", argName="columns")
   val names = opt[String](noshort=true, descr="Regexp of names of files to look for inside folders, defaults to all files. Regexp is a glob expression (see http://docs.oracle.com/javase/tutorial/essential/io/fileOps.html#glob)", argName="regexp")
   val paths = trailArg[List[String]](required=false, default=Some(DEFAULT_PATHS_TO_MINE), descr=s"Folders and files to mine, defaults to '${DEFAULT_PATHS_TO_MINE.mkString(", ")}'")
 
-  mutuallyExclusive(copyTo, copyFilteredTo)
-  mutuallyExclusive(print, printWithFileName)
+  validateOpt (copyTo, copyFilteredTo) {
+    case (None, None) => Left(s"At least '${copyTo.name}' or '${copyFilteredTo.name}' params must be set")
+    case _ => Right(Unit)
+  }
 
   validateOpt (cols) {
     case Some(l) if(l.exists(_ < 0)) => Left("All cols must be > 0")
--- src/main/scala/com/qvantel/miner/Miner.scala
+++ src/main/scala/com/qvantel/miner/Miner.scala
@@ -2,29 +2,42 @@ package com.qvantel.miner
 
 import org.rogach.scallop._
 import better.files._
+import better.files.Dsl._
 import java.io.{File => JFile}
 
 import scala.collection.mutable
+import scala.util.Try
 
 object Miner {
 
-  private def recursiveList(path: String, namesO: Option[String]): Iterator[File] = {
-    val file = File(path)
-    if(!file.isDirectory) {
-      (file :: Nil).iterator
-    } else {
-      namesO match {
-        case None => file.listRecursively
-        case Some(names) => file.glob(names)
+  /**Compute list of files to 'mine'. For each path, if it is a folder
+   * it recursively traverses it (mathing each found file with the 'names'
+   * regexp given if any), if it is a file it just adds it.
+   */
+  private def filesToMine(paths: List[String], namesO: Option[String]): List[File] = {
+
+    /**Returns files found under the given path. If a regexp is given
+     * (names param) then only files with names matching the param are returned.
+     */
+    def recursiveList(path: String, namesO: Option[String]): Iterator[File] = {
+      val file = File(path)
+      if(!file.isDirectory) {
+        (file :: Nil).iterator
+      } else {
+        namesO match {
+          case None => file.listRecursively
+          case Some(names) => file.glob(names)
+        }
       }
     }
-  }
 
-  private def filesToMine(paths: List[String], namesO: Option[String]): List[File] =
     paths.foldLeft(mutable.MutableList.empty[File]) { (acc, path) =>
       acc ++ recursiveList(path, namesO)
-    }.toList
+    }.filter(!_.isDirectory).toList
+  }
 
+  /**Returns true if it the line contains any field (line is splitted using given sep) that matches the regexp. If cols is defined only fields in those cols are checked.
+   */
   private def lineMatches(line: String, regexp: String, sep: String, colsO: Option[List[Int]]): Boolean = colsO match {
     case None => line.split(sep).exists((f:String) => f.matches(regexp))
     case Some(cols) => {
@@ -33,20 +46,52 @@ object Miner {
     }
   }
 
-  private def matchingLines(file: File, regexp: String, sep: String, colsO: Option[List[Int]]): Iterator[String] =
-    file.lineIterator.filter((l: String) => lineMatches(l, regexp, sep, colsO))
+  /** Returns all matching lines in file */
+  private def matchingLines(file: File, regexp: String, sep: String, colsO: Option[List[Int]]): Iterator[String] = {
+    Try{file.lineIterator.filter((l: String) => lineMatches(l, regexp, sep, colsO))}
+      .recover{case (_:Throwable) => List.empty[String].toIterator}.get
+  }
 
+  /** Returns true if file contains at least one matching line */
   private def containsMatchingLine(file: File, regexp: String, sep: String, colsO: Option[List[Int]]): Boolean = 
     file.lineIterator.contains((l: String) => lineMatches(l, regexp, sep, colsO))
 
+  /**'Core' of the miner, this funcion does the mining: it first computes
+   * the list of files to mine and then checks them against the regexp
+   * looked for.
+   */
   def mine(conf: Conf): Unit = {
-    val paths = conf.paths()
-    val namesO = conf.names.toOption
+    
+    /* First we get the list of files to mine */
+    val paths: List[String] = conf.paths()
+    val namesO: Option[String] = conf.names.toOption
+    val files = filesToMine(paths, namesO)
 
-    filesToMine(paths, namesO) foreach println
+    /* Now we start mining file by file */
+    val regexp: String = conf.regexp()
+    val sep: String = conf.sep()
+    val colsO: Option[List[Int]] = conf.cols.toOption
+    val copyFilteredToO: Option[String] = conf.copyTo.toOption
+    val copyToO: Option[String] = conf.copyTo.toOption
 
+    for(file <- files) {
+      /* If 'print' or 'copyFilteredTo' are set, then we are
+       * interested in the matching lines of each file, if any.
+       */
+      val destFilteredFileO = copyFilteredToO.map(destFolder => destFolder/file.name)
+      for {
+        destFile <- destFilteredFileO
+        line <- matchingLines(file, regexp, sep, colsO)
+      } destFile append line
 
+      /* If 'copy', then it is enough to know whether each
+       * file contains any mathing line or not.
+       */
+      val destFileO = copyToO.map(destFolder => destFolder/file.name)
+      for {
+        destFile <- destFileO
+        if(containsMatchingLine(file, regexp, sep, colsO))
+      } cp(file, destFile)
+    }
   }
-
 }
-
