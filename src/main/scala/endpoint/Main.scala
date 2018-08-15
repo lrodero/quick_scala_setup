f734b27f8efdc2f78eebaa26c8efeb1d7db05778
Luis Rodero-Merino <luis.rod@gmail.com>
2018-08-15 12:36:30 +0200

    Fixed the 'still open clients' issue

--- src/main/scala/endpoint/Main.scala
+++ src/main/scala/endpoint/Main.scala
@@ -8,11 +8,12 @@ import endpoint.socket.ServerSocketIO._
 import endpoint.socket.SocketIO
 import endpoint.socket.SocketIO._
 
-import cats.Eval
 import cats.effect.{Fiber, IO, IOApp}
 import cats.effect.ExitCode
 import cats.effect.ExitCase._
 import cats.effect.concurrent.{MVar, Ref}
+
+import cats.{Eval, Traverse}
 import cats.implicits._
 
 import java.io._
@@ -26,13 +27,19 @@ object MainTCP extends IOApp {
   def attendNewClient(clientSocket: Socket, stopServerFlag: MVar[IO, Unit]): IO[Unit] = {
 
     def loop(reader: BufferedReader, writer: BufferedWriter): IO[Unit] = for {
-      _    <- IO.cancelBoundary
-      line <- reader.readLineIO
-      _    <- line match {
-                case "CLOSE" => stopServerFlag.put(())
-                case ""      => IO.unit
-                case _       => writer.writeIO(line) *> writer.newLineIO *> writer.flushIO *> loop(reader, writer)
-              }
+      _     <- IO.cancelBoundary
+      lineE <- reader.readLineIO.attempt
+      _     <- lineE match {
+                 case Right(line) => line match {
+                   case "CLOSE" => stopServerFlag.put(())
+                   case ""      => IO.unit
+                   case _       => writer.writeIO(line) *> writer.newLineIO *> writer.flushIO *> loop(reader, writer)
+                 }
+                 case Left(e) => stopServerFlag.isEmpty >>= (isEmpty =>
+                                   if(!isEmpty) IO.unit // Server socket was closed, which triggered an exception on 'accept' that we ignore
+                                   else IO.raiseError(new java.lang.Error("Unexpected error", e))
+                                 )
+               }
     } yield ()
 
     def close(reader: BufferedReader, writer: BufferedWriter): IO[Unit] = 
@@ -41,12 +48,12 @@ object MainTCP extends IOApp {
     (SocketIO.getReader(clientSocket), SocketIO.getWriter(clientSocket))
       .tupled
       .bracketCase {
-        case (reader, writer) => loop(reader, writer).guarantee(close(reader, writer))
-      } { 
-        case ((reader, writer), Completed)  => IO{ println("Finished service to client normally") }
-        case ((reader, writer), Canceled)   => IO{ println(s"Finished service to client because cancellation") }
-        case ((reader, writer), Error(err)) => IO{ println(s"Finished service to client due to error: '${err.getMessage}'") }
-      }  
+        case (reader, writer) => loop(reader, writer)
+      } {
+        case ((reader, writer), Completed)  => IO{ println("Finished service to client normally") } *> close(reader, writer)
+        case ((reader, writer), Canceled)   => IO{ println("Finished service to client because cancellation") } *> close(reader, writer)
+        case ((reader, writer), Error(err)) => IO{ println(s"Finished service to client due to error: '${err.getMessage}'") } *> close(reader, writer)
+      }
   } 
 
   def serve(serverSocket: ServerSocket, stopServerFlag: MVar[IO, Unit]): IO[Unit] = {
@@ -74,9 +81,9 @@ object MainTCP extends IOApp {
     for {
       activeClients <- Ref.of[IO, Set[Fiber[IO, Unit]]](Set.empty[Fiber[IO, Unit]])
       _             <- acceptNewConnections(activeClients).guarantee{ for {
-                         clients <- activeClients.get
-                         _       <- IO { println(s"No more client connections accepted, closing ${clients.size} still active clients") }
-                         _       <- IO { clients.foreach(_.cancel) }
+                           clients <- activeClients.get
+                           _       <- IO { println(s"No more client connections accepted, closing ${clients.size} still active clients") }
+                           _       <- Traverse[List].sequence(clients.toList.map(_.cancel))
                          } yield ()
                        }
     } yield ()
@@ -97,11 +104,11 @@ object MainTCP extends IOApp {
 
     ServerSocketIO.serverSocket(Option(5002), None, None)
       .bracketCase {
-        case serverSocket => server(serverSocket).guarantee(closeServerSocket(serverSocket))
+        case serverSocket => server(serverSocket)
       } {
-        case (_, Error(err)) => IO{ println(s"Server was stopped due to an error: ${err.getMessage}") }
-        case (_, Canceled)   => IO{ println("Server execution was cancelled") }
-        case (_, Completed)  => IO{ println("Server was successfully run") }
+        case (serverSocket, Completed)  => IO{ println("Server was successfully run") } *> closeServerSocket(serverSocket)
+        case (serverSocket, Canceled)   => IO{ println("Server execution was cancelled") } *> closeServerSocket(serverSocket)
+        case (serverSocket, Error(err)) => IO{ println(s"Server was stopped due to an error: ${err.getMessage}") } *> closeServerSocket(serverSocket)
       }
   }
 
