87f2fcbd39a7d9c0fafc6b9c2cd3fd6210557d5f
Luis Rodero-Merino <luis.rod@gmail.com>
2019-03-14 23:48:55 +0100

    StateT vs Ref

--- build.sbt
+++ build.sbt
@@ -1,14 +1,18 @@
-name := "experiments"
+name := "cats-experiments"
 
 version := "0.1"
 
-scalaVersion := "2.12.2"
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-effect" % "1.2.0" withSources() withJavadoc(),
+  "org.typelevel" %% "cats-core"   % "1.5.0" withSources() withJavadoc()
+)
 
 scalacOptions ++= Seq(
   "-feature",
   "-deprecation",
   "-unchecked",
   "-language:postfixOps",
-  "-language:higherKinds")
+  "-language:higherKinds",
+  "-Ypartial-unification")
 
 
--- src/main/scala/experiments/Experiment.scala
+++ src/main/scala/experiments/Experiment.scala
@@ -1,5 +1,35 @@
 package experiments
 
-object Experiment extends App {
-  println("Now to start working...")
+import java.util.UUID
+
+import cats.data.StateT
+import cats.effect.IO
+import cats.effect.Sync
+import cats.effect.concurrent.Ref
+import cats.implicits._
+
+class Experiment[F[_]: Sync] extends App {
+
+  val status = new Status[IO, UUID, String]
+
+  val state = StateT[F, Map[String, Int], Map[String, Int]]
+
+}
+
+/**
+  * Why not StateT[F, Map, Map]? Well, because StateT is not safe for concurrent accesses
+  * as it does not provides synchronization.
+  */
+final class Status[F[_]: Sync, A, B](map: Map[A, B] = Map.empty[A,B]) {
+  val stateF: F[Ref[F, Map[A,B]]] = Ref.of[F, Map[A, B]](map)
+  def update(f: Map[A,B] => Map[A,B]): F[Unit] =
+    stateF >>= { state =>
+      state.modify { map =>
+         (f(map) , ())
+      }
+    }
+  def read[C](f: Map[A,B] => C): F[C] =
+    stateF >>= { state =>
+      state.get.map(f(_))
+    }
 }
