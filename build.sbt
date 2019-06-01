da655b4c404564e82ca89f1cb3de476218315283
Luis Rodero-Merino <luis.rod@gmail.com>
2019-06-01 15:31:44 +0200

    Keep going...

--- build.sbt
+++ build.sbt
@@ -1,8 +1,8 @@
-name := "experiments"
+name := "fs2-experiments"
 
 version := "0.1"
 
-scalaVersion := "2.12.2"
+scalaVersion := "2.12.8"
 
 scalacOptions ++= Seq(
   "-feature",
@@ -11,4 +11,9 @@ scalacOptions ++= Seq(
   "-language:postfixOps",
   "-language:higherKinds")
 
+// available for Scala 2.11, 2.12
+libraryDependencies += "co.fs2" %% "fs2-core" % "1.0.4" withSources() withJavadoc() // For cats 1.5.0 and cats-effect
+
+// optional I/O library
+libraryDependencies += "co.fs2" %% "fs2-io" % "1.0.4" withSources() withJavadoc()
 
--- src/main/scala/experiments/Experiment.scala
+++ src/main/scala/experiments/Experiment.scala
@@ -1,5 +1,33 @@
 package experiments
 
+import cats.effect.IO
+import fs2.Chunk
+import fs2.INothing
+import fs2.Pipe
+import fs2.Stream
+
 object Experiment extends App {
   println("Now to start working...")
+
+  implicit class Enriched[F[_], A](s: Stream[F, A]){
+    def myRepeatN(i: Int): Stream[F, A] = {
+      if(i <= 1) s
+      else s ++ myRepeatN(i - 1)
+    }
+    def myDrain: Stream[F, INothing] = s.mapChunks(_ => Chunk.empty)
+    def myAttempt: Stream[F, Either[Throwable, A]] = 
+      s.map(Right(_): Either[Throwable, A]).handleErrorWith(t => Stream.emit(Left(t)))
+    def myTakeWhile(cond: A => Boolean): Stream[F, A] = {
+
+      def check(a: A): Pipe[F, A, A] = ???
+
+      ???
+    }
+  }
+
+  val st = Stream.eval(IO.pure(List(1,2,3)))
+
+  println(st.myRepeatN(2).compile.fold(List.empty[Int])(_ ++ _).unsafeRunSync())
+  println(st.myRepeatN(2).myDrain.compile.fold(List.empty[Int])(_ ++ _).unsafeRunSync())
+
 }
